5673f278f902c0cbe68b5bcf5514ccfd401113ed
Andric <namdd@r12nadendla.managed.mst.edu>
2022-04-06 23:11:46 -0500

    dropout forward finished

--- mlcvlab/models/nn4.py
+++ mlcvlab/models/nn4.py
@@ -1,12 +1,42 @@
 import numpy as np
+from mlcvlab.nn.losses import l2, l2_grad
+from mlcvlab.nn.basis import linear, linear_grad
+from mlcvlab.nn.activations import relu, sigmoid, sigmoid_grad, relu_grad
+from .base import Layer
+from mlcvlab.nn.dropout import dropout, dropout_grad
 
-from mlcvlab.nn.basis import linear, radial
-from mlcvlab.nn.activations import relu
 
-def nn4(x):
-    # TODO
-    raise NotImplementedError("NN4 model not implemented")
+class NN4():
+    def __init__(self, use_batchnorm=False, dropout_param=0):
+        self.layers = [
+            Layer(None, relu),
+            Layer(None, relu),
+            Layer(None, relu),
+            Layer(None, sigmoid)]
+        
+        self.use_batchnorm = use_batchnorm
 
-def nn4_grad(y):
-    # TODO
-    raise NotImplementedError("NN4 gradient (backpropagation) not implemented")
+        #used in dropout implementation
+        self.dropout_param = dropout_param
+
+    def nn4(self, x):
+        # TODO
+        if self.use_batchnorm:
+            raise NotImplementedError("NN4 Batchnorm model not implemented")
+
+        else:
+            raise NotImplementedError("NN4 Without Batchnorm model not implemented")
+
+    def grad(self, x, y):
+        # TODO  
+        if self.use_batchnorm:
+            raise NotImplementedError("NN4 gradient (backpropagation) Batchnorm model not implemented")
+
+        else:
+            raise NotImplementedError("NN4 gradient (backpropagation) Without Batchnorm model not implemented")    
+
+    def emp_loss_grad(self, train_X, train_y, layer):
+        # emp_loss_ = 0
+        # emp_loss_grad_ = None
+        # TODO
+        raise NotImplementedError("NN4 Emperical Loss grad not implemented")
--- mlcvlab/nn/dropout.py
+++ mlcvlab/nn/dropout.py
@@ -1,12 +1,48 @@
 # No additional 3rd party external libraries are allowed
 import numpy as np
 
-def dropout(p=0.5):
-
-    # TODO
-    raise NotImplementedError("Dropout Not Implemented")
-
-def dropout_grad(p=0.5):
+def dropout(z, p, mode='test'):
+    '''
+    Output : should return a tuple containing 
+     - z : z is the output of basis function. Now it will be the output of the dropout
+     - p : Dropout param
+     - mode : 'test' or 'train'
+     - mask : 
+        - in train mode, it is the dropout mask
+        - in test mode, mask will be None.
     
-    # TODO
-    raise NotImplementedError("Gradiant of Dropout Not Implemented")
+    sample output: (z=, p=0.5, mode='test',mask=None)
+    '''
+    # TODO Implement logic for both 'test' and 'train' modes.
+    if mode == 'train':
+        # Mask for the dropout. List of binomial distribution in size of x.
+        # mask = [int(np.random.binomial(n=1, p=p, size=1)) for q in range(len(z))]
+        mask = np.random.binomial(1, p, size=z.shape) / p
+        # For every distribution == 1 in hte mask, make that element of x = 0.
+        # z_drop = [0 if mask[index] == 1 else z[index] for index in range(len(z))]
+        z_drop = z * mask
+
+    elif mode == 'test':
+        # No mask in testing mode since we want to keep all the nodes active. 
+        #What we need to do is to make it matches the training phase expectation, so we scale the layer output with p.
+        mask = None
+        z_drop = z * p
+    
+    return z_drop, p, mode, mask
+
+def dropout_grad(z, mask, mode='train'):
+    '''Gradient of the dropout.
+    Parameters:
+    - del_L_wrt_y: the gradient of loss function implemented in losses.py.
+
+    Output:  del_L_wrt_z_tilda
+    '''
+    e = 0.001
+
+    if mode == 'train':
+        # grad_L_wrt_z_tilda = 1 / mask + e
+        grad_L_wrt_z_tilda = z * mask
+    
+    return grad_L_wrt_z_tilda
+    # elif mode == 'test':
+    #     raise NotImplementedError("Gradiant of Dropout - Test Not Implemented")
