ca50074e979a23254949e53a5dcdab93ae41c3e2
NikolaAndro <dzonikika@yahoo.com>
2022-04-07 01:02:52 -0500

    finished dropout

--- mlcvlab/nn/dropout.py
+++ mlcvlab/nn/dropout.py
@@ -17,14 +17,14 @@ def dropout(z, p, mode='test'):
     if mode == 'train':
         # Mask for the dropout. List of binomial distribution in size of x.
         # mask = [int(np.random.binomial(n=1, p=p, size=1)) for q in range(len(z))]
-        mask = np.random.binomial(1, p, size=z.shape) / p
+        mask = np.random.binomial(1, p, size=z.shape) 
         # For every distribution == 1 in hte mask, make that element of x = 0.
         # z_drop = [0 if mask[index] == 1 else z[index] for index in range(len(z))]
         z_drop = z * mask
 
     elif mode == 'test':
         # No mask in testing mode since we want to keep all the nodes active. 
-        #What we need to do is to make it matches the training phase expectation, so we scale the layer output with p.
+        # What we need to do is to make it matches the training phase expectation, so we scale the layer output with p.
         mask = None
         z_drop = z * p
     
@@ -40,9 +40,9 @@ def dropout_grad(z, mask, mode='train'):
     e = 0.001
 
     if mode == 'train':
-        # grad_L_wrt_z_tilda = 1 / mask + e
-        grad_L_wrt_z_tilda = z * mask
+        # + e ?   where   e = 0.001
+        grad_D_wrt_Z_tilda = np.dot((1/mask) * np.identity(len(mask)), z)
     
-    return grad_L_wrt_z_tilda
+    return grad_D_wrt_Z_tilda
     # elif mode == 'test':
     #     raise NotImplementedError("Gradiant of Dropout - Test Not Implemented")
