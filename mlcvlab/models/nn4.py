3476c1722115d763716360f379c36df436d7fe6e
Andric <namdd@r12nadendla.managed.mst.edu>
2022-04-09 14:16:30 -0500

    nn4() done - try 1

--- mlcvlab/models/nn4.py
+++ mlcvlab/models/nn4.py
@@ -4,7 +4,7 @@ from mlcvlab.nn.basis import linear, linear_grad
 from mlcvlab.nn.activations import relu, sigmoid, sigmoid_grad, relu_grad
 from .base import Layer
 from mlcvlab.nn.dropout import dropout, dropout_grad
-
+from mlcvlab.nn.batchnorm import batchnorm, batchnorm_grad
 
 class NN4():
     def __init__(self, use_batchnorm=False, dropout_param=0):
@@ -19,13 +19,54 @@ class NN4():
         #used in dropout implementation
         self.dropout_param = dropout_param
 
-    def nn4(self, x):
+    def nn4(self, x, curent_mode):
         # TODO
         if self.use_batchnorm:
-            raise NotImplementedError("NN4 Batchnorm model not implemented")
+            #************************** LAYER 1 ************************
+            z_1_tilda = relu(np.dot(self.layers[0].W , x))
+            # apply batchnorm
+            b_1 = batchnorm(z_1_tilda, gamma=0.1, beta=0.1, eps=0.001, mode=curent_mode )
+            # apply dropout
+            y_1 = dropout(b_1, p = 0.5, mode = curent_mode)
+
+            #************************** LAYER 2 ************************
+            z_2_tilda = relu(np.dot(self.layers[1].W , y_1))
+            # apply batchnorm
+            b_2 = batchnorm(z_2_tilda, gamma=0.1, beta=0.1, eps=0.001, mode=curent_mode )
+            # apply dropout
+            y_2 = dropout(b_2, p = 0.5, mode = curent_mode)
+
+            #************************** LAYER 3 ************************
+            z_3_tilda = relu(np.dot(self.layers[2].W , y_2))
+            # apply batchnorm
+            b_3 = batchnorm(z_3_tilda, gamma=0.1, beta=0.1, eps=0.001, mode=curent_mode )
+            # apply dropout
+            y_3 = dropout(b_3, p = 0.5, mode = curent_mode)
+
+            #************************** LAYER 4 ************************
+            y_hat = sigmoid(np.dot(self.layers[3].W , y_3))
+
 
         else:
-            raise NotImplementedError("NN4 Without Batchnorm model not implemented")
+            #************************** LAYER 1 ************************
+            z_1_tilda = relu(np.dot(self.layers[0].W , x))
+            # apply dropout
+            y_1 = dropout(z_1_tilda, p = 0.5, mode = curent_mode)
+
+            #************************** LAYER 2 ************************
+            z_2_tilda = relu(np.dot(self.layers[1].W , y_1))
+            # apply dropout
+            y_2 = dropout(z_2_tilda, p = 0.5, mode = curent_mode)
+
+            #************************** LAYER 3 ************************
+            z_3_tilda = relu(np.dot(self.layers[2].W , y_2))
+            # apply dropout
+            y_3 = dropout(z_3_tilda, p = 0.5, mode = curent_mode)
+
+            #************************** LAYER 4 ************************
+            y_hat = sigmoid(np.dot(self.layers[3].W , y_3))
+
+        return y_hat
 
     def grad(self, x, y):
         # TODO  
